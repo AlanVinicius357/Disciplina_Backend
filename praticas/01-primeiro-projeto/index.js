f72306e7f62ff3e227df64b4f465c12b5c435788
AlanVinicius357 <alanvinicius295@gmail.com>
2025-08-20 21:29:46 -0300

    criando novo projeto NODE

---
+++ praticas/01-primeiro-projeto/index.js
@@ -0,0 +1,28 @@
+console.log("alo")
+
+let nome = "Alan"
+let sobreNome= "Vinicius"
+
+
+console.log(nome + " " + sobreNome)
+
+let idade = 24
+
+console.log("A idade do " + nome + " é " + idade + " anos")
+
+if (idade > 18){
+    console.log("você é maior de idade")
+}else{
+    console.log("Você é menor de idade")
+}
+
+
+let exercicios = 0.5
+
+let trabalho = 1.5
+
+let prova = 4.5
+
+let notaCalculada = exercicios + trabalho + prova
+
+console.log("Nota P1: " + notaCalculada)
