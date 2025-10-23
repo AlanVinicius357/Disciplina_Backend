e6f58b33bf1a562e614470bf7e16c2ebd8416c46
AlanVinicius357 <alanvinicius295@gmail.com>
2025-10-22 21:09:54 -0300

    novo projeto

---
+++ praticas/08-crud-pessoas/src/controllers/PessoaController.js
@@ -0,0 +1,30 @@
+const express = require('express')
+
+const router = express.Router()
+const PessoaModel = require('../models/PessoaModels')
+
+//Rotas do Crud
+
+//create
+
+
+
+//read
+
+
+
+//update
+
+
+
+//delete
+
+
+
+
+
+
+
+
+
+module.exports = router
